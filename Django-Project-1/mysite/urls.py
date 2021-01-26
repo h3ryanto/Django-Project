e63ab77048a8f94d063d42bcb38a776a9fb982f2
h3ryanto <hery.heryanto21@gmail.com>
2021-01-26 11:47:01 +0700

    menambahkan patch about pada url

--- Django-Project-1/mysite/urls.py
+++ Django-Project-1/mysite/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('about/', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
     path('admin/', admin.site.urls),
 ]
