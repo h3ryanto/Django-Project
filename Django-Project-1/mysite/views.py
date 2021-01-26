efbe6f59f27b7d8e33bd1d25cdb63072c74bedc3
h3ryanto <hery.heryanto21@gmail.com>
2021-01-26 11:48:37 +0700

    menambahakan function about pada

---
+++ Django-Project-1/mysite/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+
+def index (request):
+    return HttpResponse("Hello World! ini adalah halam index")
+
+def about (request):
+    return HttpResponse("Hello World! ini adalah halaman about")
