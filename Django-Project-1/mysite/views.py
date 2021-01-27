b191f84eed9c5cd049126290b02954d23c9c8a55
h3ryanto <hery.heryanto21@gmail.com>
2021-01-27 13:58:05 +0700

    config post profil to index.html

--- Django-Project-1/mysite/views.py
+++ Django-Project-1/mysite/views.py
@@ -1,16 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from blog.models import Profil
+
 
 def index(request):
+    profil = Profil.objects.all
     context = {
         'judul': 'Django-Project',
         'subjudul': 'selamat datang di Blog Django Project',
+        'Profil': profil,
         'nav': [
             ['/', 'Home'],
             ['/blog', 'Blog'],
             ['/about', 'About'],
-            ['/contact', 'Contact']
+            ['/contact', 'Contact'],
+
         ]
     }
     return render(request, 'index.html', context)
