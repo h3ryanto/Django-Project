8df0b4dfc71d958651be46f6ca73c3a83e0a332b
h3ryanto <hery.heryanto21@gmail.com>
2021-01-27 08:00:06 +0700

    modif views

--- Django-Project-1/mysite/views.py
+++ Django-Project-1/mysite/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    return HttpResponse("Hello World! ini adalah halaman index")
 
 
 def about(request):
