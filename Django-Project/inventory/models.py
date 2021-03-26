6c3fd3031324049b48667beeb5f719e0b52aee84
h3ryanto <hery.heryanto21@gmail.com>
2021-03-26 09:37:10 +0700

    updatestart creat code

--- Django-Project/inventory/models.py
+++ Django-Project/inventory/models.py
@@ -1,3 +1,63 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-# Create your models here.
+
+
+
+class Barang(models.Model):
+    kode_barang = models.CharField(max_length=5)
+    nama_barang = models.CharField(max_length=10)
+    satuan = models.CharField(max_length=3)
+
+    class Meta:
+        managed = False
+        db_table = 'barang'
+
+
+class BarangBackup(models.Model):
+    kode_barang = models.CharField(max_length=5)
+    nama_barang = models.CharField(max_length=10)
+    qty = models.IntegerField()
+    kategory = models.CharField(max_length=15)
+
+    class Meta:
+        managed = False
+        db_table = 'barang_backup'
+
+
+class HistoryPenjualan(models.Model):
+    kode_transaksi_pembelian = models.CharField(max_length=5)
+    kode_barang = models.CharField(max_length=5)
+    qty = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'history_penjualan'
+
+
+class Penjualan(models.Model):
+    kode_transaksi = models.IntegerField()
+    kode_barang = models.ForeignKey(Barang, models.DO_NOTHING, db_column='kode_barang')
+    qty = models.IntegerField()
+    status_history = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'penjualan'
+
+
+class TransaksiPembelian(models.Model):
+    kode_transaksi = models.CharField(max_length=5, blank=True, null=True)
+    kode_barang = models.ForeignKey(Barang, models.DO_NOTHING, db_column='kode_barang', blank=True, null=True)
+    qty = models.IntegerField(blank=True, null=True)
+    stok = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'transaksi_pembelian'
--- Django-Project/mysite/settings.py
+++ Django-Project/mysite/settings.py
@@ -74,12 +74,29 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+#sqlite
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+        'OPTIONS': {
+   'init_command': 'SET default_storage_engine=INNODB',
+   'autocommit': True,
+}
     }
 }
+
 
 
 # Password validation
@@ -118,7 +135,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 
 STATICFILES_DIRS = [
